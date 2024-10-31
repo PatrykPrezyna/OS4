7b94e4038bab9b3a85a15009a10ee5cb179841f9
PatrykPrezyna <81018358+PatrykPrezyna@users.noreply.github.com>
2024-10-31 13:26:04 +0100

    Create Data.m

---
+++ Data.m
@@ -0,0 +1,36 @@
+Battery(1).capacity = 50;
+Battery(1).cost = 6000;
+Battery(1).weight = 110;
+
+Battery(2).capacity = 190;
+Battery(2).cost = 19000;
+Battery(2).weight = 450;
+
+Battery(3).capacity = 600;
+Battery(3).cost = 57;
+Battery(3).weight = 1400;
+
+
+Chassis(1).pax = 2;
+Chassis(1).weight = 1350;
+Chassis(1).cost = 12000;
+Chassis(1).power_consumption = 140;
+
+Chassis(2).pax = 10;
+Chassis(2).weight = 2200;
+Chassis(2).cost = 31000;
+Chassis(2).power_consumption = 160;
+
+Chassis(3).pax = 30;
+Chassis(3).weight = 7000;
+Chassis(3).cost = 47000;
+Chassis(3).power_consumption = 210;
+
+
+Battery_charger(1).power = 10;
+Battery_charger(1).cost = 1000;
+Battery_charger(1).weight = 1;
+
+Battery_charger(2).power = 60;
+Battery_charger(2).cost = 7000;
+Battery_charger(2).weight = 5;
