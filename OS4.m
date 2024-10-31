6dde315e77f4cb77979c625af925a914ba50db70
PatrykPrezyna <81018358+PatrykPrezyna@users.noreply.github.com>
2024-10-31 14:18:52 +0100

    Update OS4.m

--- OS4.m
+++ OS4.m
@@ -13,11 +13,20 @@ iteration=1;
 for b=1:length(Battery)
     for c=1:length(Chassis)
         for bc=1:length(Battery_charger)
-            Design(iteration).cost = Battery(b).cost+Chassis(c).cost+Battery_charger(bc).cost;
-            Design(iteration).Battery_charge_time = Battery(b).capacity/Battery_charger(bc).power;  
-            Design(iteration).valid = 1;% simpe idea how to select dasigns ?
-            Design(iteration).Battery_charge_time
-            iteration=iteration+1;
+            for a=1:length(Autonomous_system)
+                %Cost
+                Design(iteration).cost = Battery(b).cost+Chassis(c).cost+Battery_charger(bc).cost+Autonomous_system(a).cost;
+                %SAU Availability
+                Design(iteration).Battery_charge_time = Battery(b).capacity/Battery_charger(bc).power;  
+                Design(iteration).Power_consumption = Chassis(c).power_consumption + Autonomous_system(a).power_consumption;
+                %SAU Passenger trips/hour
+                %SAU Passenger trips/day
+                %SAU Average waiting time
+                %Additional attributes
+                Design(iteration).autonomy_level = Autonomous_system(a).level;
+                Design(iteration).valid = 1;% simpe idea how to select dasigns ?
+                iteration=iteration+1;
+            end
         end
     end
 end
