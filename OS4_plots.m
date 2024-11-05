8604073c92e53fb64ee84bbe0408f9cf398f7077
patrykprezyna <patryk.prezyna@rommelag.com>
2024-11-05 14:25:54 +0100

    Improve on the bike design

--- OS4_plots.m
+++ OS4_plots.m
@@ -8,19 +8,16 @@ pareto_cost = [1000000000];
 
 for i=1:length(Design)
     x(i) = Design(i).cost;
-    y(i) = Design(i).SAU_availability;
-    topic = "SAU_availability: ";
-    % y(i) = Design(i).Peak_Passenger_Throughput;
-    % topic = "Peak_Passenger_Throughput: ";
+    %y(i) = Design(i).SAU_availability;
+    %topic = "SAU_availability: ";
+    %y(i) = Design(i).SAU_Peak_Passenger_Throughput;
+    %topic = "Peak_Passenger_Throughput: ";
     % y(i) = Design(i).SAU_Passenger_Volume;
     % topic = "SAU_Passenger_Volume: ";   
     %y(i) = Design(i).SAU_Wait_Time;
     %topic = "SAU_Wait_Time: ";
-    %y(i) = Design(i).MAU;
-    %topic = "MAU: ";
-
-
-
+    y(i) = Design(i).MAU;
+    topic = "MAU: ";
     
     %design is dominated
     dominated = 0;
@@ -54,7 +51,7 @@ for i=1:length(Design)
     color(1,i)=Design(i).Battery_capacity;
     Design_variable_names = "Battery capacity";
     color(2,i)= Design(i).Chassis_pax;
-    Design_variable_names = cat(1, Design_variable_names, ["Chassis_pax"]);
+    Design_variable_names = cat(1, Design_variable_names, ["Chassis Pax"]);
     color(3,i)=Design(i).Battery_charger_power;
     Design_variable_names = cat(1, Design_variable_names, ["Battery_charger_power"]);
     color(4,i)=Design(i).autonomy_level;
@@ -63,6 +60,17 @@ for i=1:length(Design)
     Design_variable_names = cat(1, Design_variable_names, ["Motor_power"]);
     color(6,i)=Design(i).Number_of_vehicles;
     Design_variable_names = cat(1, Design_variable_names, ["Number_of_vehicles"]);
+    %Design variable EB
+    color(7,i)=Design(i).Number_of_bikes;
+    Design_variable_names = cat(1, Design_variable_names, ["Number_of_bikes"]);
+    color(8,i)=Design(i).Battery_capacity_EB;
+    Design_variable_names = cat(1, Design_variable_names, ["Battery capacity bike"]);
+    color(9,i)= Design(i).Frame_weight_EB;
+    Design_variable_names = cat(1, Design_variable_names, ["Frame_weight_EB"]);
+    color(10,i)=Design(i).Battery_charger_power_EB;
+    Design_variable_names = cat(1, Design_variable_names, ["Battery_charger_power_EB"]);
+    color(11,i)=Design(i).Motor_power_EB;
+    Design_variable_names = cat(1, Design_variable_names, ["Motor_power_EB"]);
 end
  size=zeros(length(color),1)+20;
  %shape=zeros(length(color),1)+20;
@@ -71,7 +79,7 @@ end
  end
 
 %SAU Fleet Figure
-for i=1:6
+for i=1:11%1:12
     figure
     %idea for more plots on one figures
     % t = tiledlayout(2,2);
@@ -91,11 +99,11 @@ for i=1:6
     %c.TickLabels = {'50000\[kW]','190000\[kW]','600000\[kW]'};
     
     colormap(jet);
-    xlabel('Vehicle Cost [$]');
+    xlabel('Cost [$]');
     ylim([0 1]);
     ylabel('Utility');
     xlim([0 max(x)]);
-    xlim([0 500000]);%
+    %xlim([0 500000]);%
     ylabel(colorbar,Design_variable_names(i));
     %topic = 'One vehicle Availability: ';
     title(append(topic, Design_variable_names(i)));
@@ -120,9 +128,9 @@ for i=1:length(unique_pareto)
     Pareto_Designs_EB = cat(1, Pareto_Designs_EB, [unique_pareto(i) Design(unique_pareto(i)).Number_of_bikes Design(unique_pareto(i)).Battery_capacity_EB Design(unique_pareto(i)).Frame_weight_EB Design(unique_pareto(i)).Battery_charger_power_EB]);
 end
 display("Design | Number_of_vehicles | Battery_capacity | Chassis_pax | Battery_charger_power | Auto level")
-Pareto_Designs;
+Pareto_Designs
 display("Design | Number_of_BIKES | Battery_capacity | Frame_weight | Battery_charger_power")
-Pareto_Designs_EB;
+Pareto_Designs_EB
 
 %DEREKS CODE
 % EB_MAU=[Designs_EB.MAU];
